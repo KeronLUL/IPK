acde16856332f71e70ce71decf334c0e7c16d46d
TheKeron <k.norek@gymik.cz>
2021-04-22 18:43:14 +0200

    tcp and udp

--- ipk-sniffer.cpp
+++ ipk-sniffer.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <iomanip>
+#include <ctype.h>
 #include <getopt.h>
 #include <cstring>
+#include <time.h> 
 #include <pcap/pcap.h>
-#include <signal.h>
+#include <net/ethernet.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
+
+
+#define SIZE_ETHERNET 14
 
 class ArgumentParser {
     public:
@@ -15,104 +26,104 @@ class ArgumentParser {
         bool icmp = false;
         bool showInterface = false;
     
-    void printHelp(){
-        std::cout << "pepga\n";
-    }
-
-    void argumentParser(int argc, char *argv[]){
-        const char* const shortOps = ":i:p:tun:;";
-        const struct option longOpts[] = {
-            {"interface", optional_argument, nullptr, 'i'},
-            {"tcp", no_argument, nullptr, 't'},
-            {"udp", no_argument, nullptr, 'u'},
-            {"arp", no_argument, nullptr, 0},
-            {"icmp", no_argument, nullptr, 0},
-            {"help", no_argument, nullptr, 'h'},
-            {nullptr, 0, nullptr, 0}
-        };
-
-        if (argc == 1){
-            showInterface = true;
-            return;
+        void printHelp(){
+            std::cout << "pepga\n";
         }
 
-        int opt = 0, optionIndex;
-        while ((opt = getopt_long(argc, argv, shortOps, longOpts, &optionIndex)) != EOF) {
-            switch (opt) {
-                case 'i':
-                    interface = optarg;
-                    break;
-                case ':':
-                    showInterface = true;
-                    break;
-                case 'p':
-                    try{
-                        port = std::stoi(optarg);
-                        if (port < 0 || port > 65535) {
-                            std::cerr << "Invalid argument " << optarg << "\n";
-                        }
-                    }
-                    catch (const std::invalid_argument& ia){
-                        std::cerr << "Invalid argument " << optarg << "\n";
-                        
-                        exit(1);
-                    }
-                    catch (const std::out_of_range& oor){
-                        std::cerr << "Argument " << optarg << " out of range\n";
-                        exit(1);
-                    }
-                    break;
-                case 'n':
-                    try{
-                        n = std::stoi(optarg);
-                        if (n <= 0 ) {
-                            std::cerr << "Invalid argument " << optarg << "\n";
-                        }
-                    }
-                    catch (const std::invalid_argument& ia){
-                        std::cerr << "Invalid argument " << optarg << "\n";
-                        exit(1);
-                    }
-                    catch (const std::out_of_range& oor){
-                        std::cerr << "Argument " << optarg << " out of range\n";
-                        exit(1);
-                    }
-                    break;
-                case 't':
-                    tcp = true;
-                    break;
-                case 'u':
-                    udp = true;
-                    break;
-                case 0:
-                    if (longOpts[optionIndex].flag != 0){
-                        break;
-                    }
-                    if (strcmp(longOpts[optionIndex].name, "arp") == 0){
-                        arp = true;
-                        break;
-                    }else if (strcmp(longOpts[optionIndex].name, "icmp") == 0){
-                        icmp = true;
-                        break;
-                    }
-                    break;
-                case 'h':
-                    printHelp();
-                    exit(0);
-                case '?':
-                default:
-                    std::cerr << "Invalid arguments\n";
-                    exit(1);
+        int argumentParser(int argc, char *argv[]){
+            const char* const shortOps = ":i:p:tun:;";
+            const struct option longOpts[] = {
+                {"interface", optional_argument, nullptr, 'i'},
+                {"tcp", no_argument, nullptr, 't'},
+                {"udp", no_argument, nullptr, 'u'},
+                {"arp", no_argument, nullptr, 0},
+                {"icmp", no_argument, nullptr, 0},
+                {"help", no_argument, nullptr, 'h'},
+                {nullptr, 0, nullptr, 0}
+            };
+
+            if (argc == 1){
+                showInterface = true;
+                return 0;
             }
+
+            int opt = 0, optionIndex;
+            while ((opt = getopt_long(argc, argv, shortOps, longOpts, &optionIndex)) != EOF) {
+                switch (opt) {
+                    case 'i':
+                        interface = optarg;
+                        break;
+                    case ':':
+                        showInterface = true;
+                        break;
+                    case 'p':
+                        try{
+                            port = std::stoi(optarg);
+                            if (port < 0 || port > 65535) {
+                                std::cerr << "Invalid argument " << optarg << "\n";
+                            }
+                        }
+                        catch (const std::invalid_argument& ia){
+                            std::cerr << "Invalid argument " << optarg << "\n";
+                            return 1;
+                        }
+                        catch (const std::out_of_range& oor){
+                            std::cerr << "Argument " << optarg << " out of range\n";
+                            return 1;
+                        }
+                        break;
+                    case 'n':
+                        try{
+                            n = std::stoi(optarg);
+                            if (n <= 0 ) {
+                                std::cerr << "Invalid argument " << optarg << "\n";
+                            }
+                        }
+                        catch (const std::invalid_argument& ia){
+                            std::cerr << "Invalid argument " << optarg << "\n";
+                            return 1;
+                        }
+                        catch (const std::out_of_range& oor){
+                            std::cerr << "Argument " << optarg << " out of range\n";
+                            return 1;
+                        }
+                        break;
+                    case 't':
+                        tcp = true;
+                        break;
+                    case 'u':
+                        udp = true;
+                        break;
+                    case 0:
+                        if (longOpts[optionIndex].flag != 0){
+                            break;
+                        }
+                        if (strcmp(longOpts[optionIndex].name, "arp") == 0){
+                            arp = true;
+                            break;
+                        }else if (strcmp(longOpts[optionIndex].name, "icmp") == 0){
+                            icmp = true;
+                            break;
+                        }
+                        break;
+                    case 'h':
+                        printHelp();
+                        return 0;
+                    case '?':
+                    default:
+                        std::cerr << "Invalid arguments\n";
+                        return 1;
+                }
+            }
+            if (interface != "" && showInterface){
+                std::cerr << "Invalid arguments\n";
+                return 1;
+            }
+            return 0;
         }
-        if (interface != "" && showInterface){
-            std::cerr << "Invalid arguments\n";
-            exit(1);
-        }
-    }
 };
 
-void printDevices(){
+int printDevices(){
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_if_t* interfaces;
 
@@ -123,84 +134,221 @@ void printDevices(){
         pcap_freealldevs(interfaces);
     } else {
         std::cerr << errbuf << "\n";
-        exit(1);
+        return 1;
     }
+    return 0;
+}
+
+void printHeaderIP(const struct pcap_pkthdr *header, const struct ip *iph, const u_char *packet, u_int8_t type){
+    std::string src = inet_ntoa(iph->ip_src);
+    std::string dst = inet_ntoa(iph->ip_dst);
+    std::string srcPort;
+    std::string dstPort;
+    if (type == IPPROTO_TCP){
+        struct tcphdr* tcph = (struct tcphdr*)(packet + (iph->ip_hl * 4) + SIZE_ETHERNET);
+        srcPort = std::to_string(ntohs(tcph->source));
+        dstPort = std::to_string(ntohs(tcph->dest));
+            
+    }else if (type == IPPROTO_UDP){
+        struct udphdr* udph = (struct udphdr*)(packet + (iph->ip_hl * 4) + SIZE_ETHERNET);
+        srcPort = std::to_string(ntohs(udph->source));
+        dstPort = std::to_string(ntohs(udph->dest));
+    }
+
+    time_t time_sec = header->ts.tv_sec;
+    const struct tm* locTime = localtime(&time_sec);
+    const struct tm* globTime = gmtime(&time_sec);
+    int i = (globTime->tm_hour) - locTime->tm_hour;
+    std::string timezone;
+    if (i == 0){
+        timezone = "Z";
+    }else timezone = std::to_string(i);
+
+    std::string time = std::to_string(locTime->tm_hour) + ":" + std::to_string(locTime->tm_min) + ":" + std::to_string(locTime->tm_sec) + "." + std::to_string(header->ts.tv_usec) + timezone;
+    std::string year = std::to_string(locTime->tm_year + 1900) + "-" + std::to_string(locTime->tm_mon + 1) + "-" + std::to_string(locTime->tm_mday) + "T";
+    std::string timestamp = year + time;
+
+    std::cout << timestamp << " " << src << " : " << srcPort << " > " << dst << " : " << dstPort << ", length " << header->len << " bytes" << "\n\n";
+}
+
+void print_hex_ascii_line(const u_char *packet, size_t len, int offset){
+	size_t gap;
+	const u_char *character;
+
+    std::cout << "0x" << std::setfill('0') << std::setw(4) << std::hex << offset << ": ";
+    
+	character = packet;
+	for(size_t i = 0; i < len; i++) {
+        std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)(unsigned char)*character << " ";
+		character++;
+        if (i == 7){
+            std::cout << " ";
+        }
+	}
+
+	/* print space to handle line less than 8 bytes */
+	if (len < 8)
+		std::cout << " ";
+
+	if (len < 16) {
+		gap = 16 - len;
+		for (size_t  i = 0; i < gap; i++) {
+			std::cout << "   ";
+		}
+	}
+	std::cout << " ";
+
+	character = packet;
+	for(size_t i = 0; i < len; i++) {
+		if (isprint(*character))
+            std::cout << *character;
+		else
+			std::cout << ".";
+		character++;
+	}
+	std::cout << std::endl;
+    return;
+}
+
+void printPacket(const u_char *payload, size_t len){
+	size_t len_rem = len;
+	size_t line_width = 16;			
+	size_t line_len;
+	int offset = 0;					
+	const u_char *ch = payload;
+
+	if (len != 0){
+        if (len <= line_width) {
+            print_hex_ascii_line(ch, len, offset);
+            return;
+        }
+
+        while(true) {
+            line_len = line_width % len_rem;
+            print_hex_ascii_line(ch, line_len, offset);
+            len_rem = len_rem - line_len;
+            ch = ch + line_len;
+            offset = offset + line_width;
+
+            if (len_rem <= line_width) {
+                print_hex_ascii_line(ch, len_rem, offset);
+                break;
+            }
+        }
+    }
+    std::cout << std::endl;
+    return;
 }
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
-    const size_t size = header->len;
-    std::cout << size << "\n";
+    struct ether_header *p = (struct ether_header *) packet;
+	if (p->ether_type != ETHERTYPE_IPV6) {
+        struct ip *iph = (struct ip*)(packet + SIZE_ETHERNET);
+        size_t size = header->len;
+       
+        if (iph->ip_p == IPPROTO_TCP){
+            printHeaderIP(header, iph, packet, iph->ip_p);
+            printPacket(packet, size);
+        }else if (iph->ip_p == IPPROTO_UDP){
+            printHeaderIP(header, iph, packet, iph->ip_p);
+            printPacket(packet, size);
+        }else if (iph->ip_p == IPPROTO_ICMP){
+            printHeaderIP(header, iph, packet, iph->ip_p);
+            printPacket(packet, size);
+        }
+    }
 }
 
 std::string buildFilter(ArgumentParser args){
-    std::string filter = "(ip or ip6)";
-
-    if (args.port != -1) {
-        filter += " and port " + std::to_string(args.port);
-    }
-    if (args.tcp) {
-        filter += " and (tcp";
-    }
-    if (args.udp) {
-        filter += " or udp";
-    }
+    std::string filter = "";
+    
     if (args.arp) {
-        filter += " or arp";
+        filter += "(arp";
     }
     if (args.icmp) {
-        filter += " or (icmp or icmp6))";
-    }else filter += ")";
+        if (args.arp){
+            filter += " or (icmp or icmp6))";
+        }else filter += "(icmp or icmp6)";
+    }
+    if (args.port != -1) {
+        if (args.icmp || args.arp){
+            filter += " or port " + std::to_string(args.port);
+        }else filter += "port " + std::to_string(args.port);
+    }
+    if (args.tcp) {
+        if (args.port != -1){
+            filter += " and (tcp";
+        }else filter += "(tcp";
+    }
+
+    if (args.udp) {
+        if (args.tcp) {
+            filter += " or udp";
+        }else if (args.port != -1){
+            filter += " and (udp";
+        }else filter += "(udp";
+    }
+    
+    if (args.tcp || args.udp){
+        filter += ")";
+    }
 
     return filter;
 }
 
-void runSniffer(ArgumentParser args){
+int runSniffer(ArgumentParser args){
     char errbuf[PCAP_ERRBUF_SIZE];
     const std::string filter = buildFilter(args);
     struct bpf_program fp;
     pcap_t* handle; 
     bpf_u_int32 mask;
     bpf_u_int32 net;
-    
+
     if (pcap_lookupnet(args.interface.c_str(), &net, &mask, errbuf) == -1) {
-        std::cerr << "Couldn't get netmask for device " << args.interface << ": " << errbuf << "\n";
+        std::cerr << errbuf << "\n";
         net = 0;
         mask = 0;
     }
+
     if ((handle = pcap_open_live(args.interface.c_str(), BUFSIZ, 1, 1000, errbuf)) == nullptr){
         std::cerr << errbuf << "\n";
-        exit(1);
+        return 1;
     }
     if (pcap_compile(handle, &fp, filter.c_str(), 0, net) == -1) {
         std::cerr << "Couldn't parse filter " << filter << ": " << pcap_geterr(handle) << "\n";
         pcap_close(handle);
-        exit(1);
+        return 1;
     }
     if (pcap_setfilter(handle, &fp) == -1) {
         std::cerr << "Couldn't install filter " << filter << ": " << pcap_geterr(handle) << "\n";
         pcap_freecode(&fp);
         pcap_close(handle);
-        exit(1);
+        return 1;
     }
 
     if (pcap_loop(handle, args.n, got_packet, nullptr) == PCAP_ERROR){
-        exit(1);
+        return 1;
     }
 
     pcap_freecode(&fp);
     pcap_close(handle);
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
     ArgumentParser args;
-    args.argumentParser(argc, argv);
-
+    if (args.argumentParser(argc, argv)){
+        return 1;
+    }
     if (args.showInterface) {
-        printDevices();
+        if (printDevices()){
+            return 1;
+        }
         return 0;
     }
-
-    runSniffer(args);
+    if (runSniffer(args)){
+        return 1;
+    }
 
     return 0;
 }
